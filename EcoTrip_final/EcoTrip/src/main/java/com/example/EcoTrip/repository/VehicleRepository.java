e90605e1f2b3dc0f24616b027beedf06486fa653
Shashini19 <jayarat-ps21002@stu.kln.ac.lk>
2026-02-12 14:47:58 +0530

    Remove all files from login-feature

--- EcoTrip_final/EcoTrip/src/main/java/com/example/EcoTrip/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcoTrip.repository;
-
-import com.example.EcoTrip.model.Vehicle;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface VehicleRepository extends MongoRepository<Vehicle, String> {
-    List<Vehicle> findByUserId(String userId);
-}
--- EcoTrip_final/EcoTrip/src/main/java/com/example/EcoTrip/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcoTrip.service;
-
-import com.example.EcoTrip.dto.SignupRequest;
-import com.example.EcoTrip.dto.LoginRequest;
-
-public interface AuthService {
-    String signup(SignupRequest request);
-    String login(LoginRequest request);
-}
