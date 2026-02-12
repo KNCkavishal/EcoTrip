89104185ec4be09c81a18ffc934fd51b8f90f70e
Shashini19 <jayarat-ps21002@stu.kln.ac.lk>
2026-02-12 14:47:03 +0530

    Remove all files from branch_hirumi

--- EcoTrip_final/EcoTrip/src/main/java/com/example/EcoTrip/repository/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcoTrip.repository;
-
-import com.example.EcoTrip.model.Auth;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface AuthRepository extends MongoRepository<Auth, String> {
-    Optional<Auth> findByUserId(String userId);
-}
--- EcoTrip_final/EcoTrip/src/main/java/com/example/EcoTrip/repository/FamilyTripRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EcoTrip.repository;
-
-import com.example.EcoTrip.model.FamilyTrip;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FamilyTripRepository extends MongoRepository<FamilyTrip, String> {
-}
