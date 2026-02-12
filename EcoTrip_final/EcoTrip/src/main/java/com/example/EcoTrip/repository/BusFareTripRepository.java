bf0cef3d70926936204a924f3a67cfdb07b96ce0
Ishani Buddhima <buddhim-ps21158@stu.kln.ac.lk>
2026-02-12 14:20:39 +0530

    Hotel.java

--- EcoTrip_final/EcoTrip/src/main/java/com/example/EcoTrip/repository/BusFareTripRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EcoTrip.repository;
-
-import com.example.EcoTrip.model.BusFareTrip;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BusFareTripRepository extends MongoRepository<BusFareTrip, String> {
-}
